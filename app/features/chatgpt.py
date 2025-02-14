11b8ee83dba9444980084c335e5c8ec402ef657d
khang200923 <92591231+khang200923@users.noreply.github.com>
2025-02-14 23:39:21 +0700

    argh f-strings

--- app/features/chatgpt.py
+++ app/features/chatgpt.py
@@ -14,13 +14,13 @@ def chat(app: App):
         ack()
         if command["user_id"] != os.getenv("THE_CREATOR_ID"):
             ack()
-            say(f"<@{command["user_id"]}> is not authorized to use /halchat. Yo <@{os.getenv("THE_CREATOR_ID")}> c'mere.")
+            say(f"<@{command['user_id']}> is not authorized to use /halchat. Yo <@{os.getenv('THE_CREATOR_ID')}> c'mere.")
         res = llm.create(
             model="gpt-4o-mini-2024-07-18",
             messages=[llm.userp(command["text"])],
             max_tokens=3000
         ).choices[0].message.content
-        say(f"<@{os.getenv("THE_CREATOR_ID")}> asks \"{command['text']}\". HAL says:\n\n{res}")
+        say(f"<@{os.getenv('THE_CREATOR_ID')}> asks \"{command['text']}\". HAL says:\n\n{res}")
     return handle_chat
 
 def feature(app: App):
